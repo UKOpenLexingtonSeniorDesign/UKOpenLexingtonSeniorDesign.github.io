af01de0e06901a9b02ecb09fa650eeb6faa80f18
adhe223 <adhe223@g.uky.edu>
2014-03-03 00:27:14 -0500

    Tried Some stuff
    Same Error

--- Android/BusAlert/src/io/github/ukopenlexingtonseniordesign/busalert/MainActivity.java
+++ Android/BusAlert/src/io/github/ukopenlexingtonseniordesign/busalert/MainActivity.java
@@ -1,7 +1,8 @@
 package io.github.ukopenlexingtonseniordesign.busalert;
 
 import android.os.Bundle;
-
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.app.Activity;
 import android.view.Menu;
 
