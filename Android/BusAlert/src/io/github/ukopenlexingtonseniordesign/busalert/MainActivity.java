1ea4ca69e8bf96926156908b483c2c53f4afb33a
adhe223 <adhe223@g.uky.edu>
2014-03-03 14:23:14 -0500

    Simple Map Embed Working
    To Do Next:
    -Popup menu for the subscription info

--- Android/BusAlert/src/io/github/ukopenlexingtonseniordesign/busalert/MainActivity.java
+++ Android/BusAlert/src/io/github/ukopenlexingtonseniordesign/busalert/MainActivity.java
@@ -1,9 +1,9 @@
 package io.github.ukopenlexingtonseniordesign.busalert;
 
+import android.app.Activity;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
