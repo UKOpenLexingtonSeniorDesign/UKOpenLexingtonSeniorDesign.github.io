8c21154cb37cf41746db04c32caf5bb380745a5a
adhe223 <adhe223@g.uky.edu>
2014-03-08 20:20:01 -0500

    Tweaked the look
    Added padding

---
+++ _site/Android/BusAlert/gen/io/github/ukopenlexingtonseniordesign/busalert/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package io.github.ukopenlexingtonseniordesign.busalert;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
