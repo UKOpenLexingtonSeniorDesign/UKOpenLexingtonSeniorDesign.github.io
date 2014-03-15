d0d5380f2089e681d3af646a109bc87a816a64a7
adhe223 <adhe223@g.uky.edu>
2014-03-15 17:14:32 -0400

    Removed Android Folder

--- Android/BusAlert/gen/io/github/ukopenlexingtonseniordesign/busalert/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package io.github.ukopenlexingtonseniordesign.busalert;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
